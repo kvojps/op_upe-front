cd975430122a4199bf7c3f186a3e8d9a6d9862a4
José Ferreira <josebazjunior09@gmail.com>
2023-04-07 22:06:10 -0300

    refatorando tela de contato

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import { ContactContainer, ContactContent, ContactContentButton, ContactContentForm, ContactContentH2, ContactContentH3, ContactContentInput, ContactContentInputTextarea } from "./styles";
+import { ContactContainer, ContactContent, ContactContentButton, ContactContentForm, ContactContentH2, ContactContentH3, ContactContentInfo, ContactContentInput, ContactContentInputTextarea } from "./styles";
+import { Question } from "@phosphor-icons/react";
 
 export function Contact() {
     const [nome, setNome] = useState("");
@@ -11,7 +12,6 @@ export function Contact() {
 
     return (
         <ContactContainer>
-            <h1>Contato</h1>
             <ContactContent>
                 <ContactContentForm action="https://formsubmit.co/joseferreira.santosjunior@upe.br" method="POST">
                     <span>Primeiro Nome *</span>
@@ -20,20 +20,18 @@ export function Contact() {
                     <ContactContentInput type="email" name="email" placeholder="E.x. John@doe.com" onChange={(event) => setEmail(event.target.value)} required />
                     <span>Telefone</span>
                     <ContactContentInput type="tel" name="telefone" placeholder="E.x. 11 11111-1111" onChange={(event) => setTelefone(event.target.value)} />
-                    <ContactContentInputTextarea name="email" placeholder="Mensagem"  onChange={(event) => setMensagem(event.target.value)} />
-                    <ContactContentButton type="submit" disabled={nome.length < 4 || email.length < 10} onClick={()=>{}}>Enviar Mensagem</ContactContentButton>
+                    <ContactContentInputTextarea name="email" placeholder="Mensagem" onChange={(event) => setMensagem(event.target.value)} />
+                    <ContactContentButton type="submit" disabled={nome.length < 4 || email.length < 10} onClick={() => { }}>Enviar Mensagem</ContactContentButton>
                 </ContactContentForm>
-                <div>
+                <ContactContentInfo>
                     <ContactContentH3>ENTRE EM CONTATO COM O SUPORTE</ContactContentH3>
                     <ContactContentH2>Estamos felizes em falar com você, apenas complete o formulário.</ContactContentH2>
                     <p>Faremos de tudo para atender você o mais rápido possível, fique no aguardo!</p>
-                    <h1>Segunda - Sexta</h1>
-                    <h1>8:00AM - 5:00PM</h1>
-                </div>
+                    <div>
+                        <Question size={250} color="#dd6b20" />
+                    </div>
+                </ContactContentInfo>
             </ContactContent>
-
-
-            
         </ContactContainer>
     )
 }
