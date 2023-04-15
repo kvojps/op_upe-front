576dc4a65e713d7472b57e6a7405e2e7b8d59991
rafa-souza-dev <rafiradeveloper@gmail.com>
2023-04-14 23:14:32 -0300

    chore: added nonempty property

--- src/validation/form-schemas.ts
+++ src/validation/form-schemas.ts
@@ -1,6 +1,11 @@
 import * as zod from 'zod'
 
 export const SignInFormValidationSchema = zod.object({
-    email: zod.string().email('E-mail inválido.'),
-    password: zod.string().min(8, 'A senha precisa de no mínimo 8 dígitos.').max(16, 'A senha pode ter no máximo 16 dígitos'),
+    email: zod.string()
+        .nonempty('Preencha o campo de E-mail.')
+        .email('E-mail inválido.'),
+    password: zod.string()
+        .nonempty('Preencha o campo de senha.')
+        .min(8, 'A senha precisa de no mínimo 8 dígitos.')
+        .max(16, 'A senha pode ter no máximo 16 dígitos'),
 })
